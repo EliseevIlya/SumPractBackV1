9f17a216b2256e8ef3c6610d7362ffcbb8f41032
crazyfrequency <di.efimoff2015@gmail.com>
2024-07-14 03:16:53 +0400

    remake filters

--- src/main/java/com/example/sumpractbackv1/model/dto/search/AccRstrListSearchCriteria.java
+++ src/main/java/com/example/sumpractbackv1/model/dto/search/AccRstrListSearchCriteria.java
@@ -1,6 +1,9 @@
 package com.example.sumpractbackv1.model.dto.search;
 
 import com.example.sumpractbackv1.model.enums.AccRstr;
+
+import io.swagger.v3.oas.annotations.Parameter;
+import jakarta.validation.constraints.*;
 import lombok.*;
 
 import java.time.LocalDate;
@@ -15,8 +18,21 @@ import org.springdoc.core.annotations.ParameterObject;
 @ToString
 @ParameterObject
 public class AccRstrListSearchCriteria extends BaseEntitySearchCriteria {
+
+    @Parameter(description = "Код ограничений операций по счету")
     private AccRstr accRstr;
-    private LocalDate accRstrDate;
-    private Long successorBIC;
+
+    @Parameter(description = "Параметр для поиска даты начала действия ограничения операций по счету")
+    private LocalDate fromAccRstrDate;
+
+    @Parameter(description = "Параметр для поиска даты начала действия ограничения операций по счету")
+    private LocalDate toAccRstrDate;
+
+    @Parameter(description = "БИК преемника")
+    @Digits(integer = 9, fraction = 0)
+    private String searchSuccessorBIC;
+
+    @Parameter(description = "Id Accounts")
+    @Positive(message = "Id Accounts должен быть больше нуля")
     private Long accountsId;
 }
---
+++ src/main/java/com/example/sumpractbackv1/util/specifications/StaticUtils.java
@@ -0,0 +1,23 @@
+package com.example.sumpractbackv1.util.specifications;
+
+import jakarta.persistence.criteria.*;
+
+public class StaticUtils {
+
+	public static String likePattern(String value) {
+		return "%" + value + "%";
+	}
+
+	public static Predicate likeBic(CriteriaBuilder criteriaBuilder, Expression<?> x, String value) {
+		return criteriaBuilder.like(
+			criteriaBuilder.function(
+				"to_char",
+				String.class,
+				x,
+				criteriaBuilder.literal("000000000")
+			),
+			likePattern(value)
+		);
+	}
+	
+}
