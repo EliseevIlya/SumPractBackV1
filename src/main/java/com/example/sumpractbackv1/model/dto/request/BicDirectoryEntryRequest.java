09022be1e6b6b3054ef0564973f5f297ebfce010
GhostDragon <bk.if3@ya.ru>
2024-07-16 21:02:47 +0400

    BicDirectoryEntryRequest Schema commit

--- src/main/java/com/example/sumpractbackv1/model/dto/request/BicDirectoryEntryRequest.java
+++ src/main/java/com/example/sumpractbackv1/model/dto/request/BicDirectoryEntryRequest.java
@@ -10,16 +10,16 @@ import java.util.List;
 @Schema(description = "класс для создания/изменение BICDirectoryEntry. Запись в Справочнике БИК.")
 public class BicDirectoryEntryRequest extends BaseEntityRequest {
 
-    @Schema
+    @Schema(description = "БИК.")
     @Size(min = 9, max = 9)
     @NotNull
     private Long bic;
 
-    @Schema
+    @Schema(description = "Тип изменения в Справочнике по сравнению с предыдущей версией cправочника (добавление записи/изменение/удаление).")
     @Size(min = 4, max = 4)
     private ChangeType changeType;
 
-    @Schema
+    @Schema(description = "")
     private Long importData;
 
     @Schema
